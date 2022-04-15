1383a6c33e068b95e6c4eef2b9cbd70adb916091
banku27 <pankajparihar123321@gmail.com>
2022-04-15 22:57:46 +0530

    updated homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import 'package:instagram_clone/constants/story_json.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -17,19 +19,33 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: getAppBar(),
+      backgroundColor: Colors.black,
+      body: getBody(),
+      bottomNavigationBar: bottomTab(),
+    );
+  }
+
   Widget bottomTab() {
     List bottomItems = [
-      pageIndex == 0 ? "assets/home_active_icon.svg" : "assets/home_icon.svg",
+      pageIndex == 0
+          ? "assets/images/home_active_icon.svg"
+          : "assets/images/home_icon.svg",
       pageIndex == 1
-          ? "assets/search_active_icon.svg"
-          : "assets/search_icon.svg",
+          ? "assets/images/search_active_icon.svg"
+          : "assets/images/search_icon.svg",
       pageIndex == 2
-          ? "assets/upload_active_icon.svg"
-          : "assets/upload_icon.svg",
-      pageIndex == 3 ? "assets/love_active_icon.svg" : "assets/love_icon.svg",
+          ? "assets/images/upload_active_icon.svg"
+          : "assets/images/upload_icon.svg",
+      pageIndex == 3
+          ? "assets/images/love_icon.svg"
+          : "assets/images/love_active_icon.svg",
       pageIndex == 4
-          ? "assets/account_active_icon.svg"
-          : "assets/account_icon.svg",
+          ? "assets/images/account_active_icon.svg"
+          : "assets/images/account_icon.svg",
     ];
     return Container(
       width: double.infinity,
@@ -55,11 +71,85 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      bottomNavigationBar: bottomTab(),
+  getAppBar() {
+    return AppBar(
+      backgroundColor: Colors.black,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SvgPicture.asset(
+            'assets/images/camera_icon.svg',
+            width: 30,
+          ),
+          const Text(
+            'Instagram',
+            style: TextStyle(
+                fontFamily: 'Billabong', fontSize: 35, letterSpacing: 1),
+          ),
+          SvgPicture.asset(
+            "assets/images/message_icon.svg",
+            width: 30,
+          ),
+        ],
+      ),
+    );
+  }
+
+  getBody() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: List.generate(
+                stories.length,
+                (index) => Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(left: 8, top: 10),
+                      width: 68,
+                      height: 68,
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [Color(0xFF9B2282), Color(0xFFEEA863)],
+                        ),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Container(
+                          height: 62,
+                          width: 62,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 2),
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image: NetworkImage(stories[index]['img']),
+                                fit: BoxFit.cover),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    SizedBox(
+                      width: 80,
+                      child: Text(
+                        stories[index]['name'],
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
